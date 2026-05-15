b138758ea92a8e6f3fcc05d882fb78a95533569b
Ari Kurniawan <ari.kurniawan@seatrium.com>
2026-05-15 09:36:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  base: '',
+  base: process.env.GITHUB_ACTIONS ? '/photobooth/' : '/',
   plugins: [devtools(), solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
